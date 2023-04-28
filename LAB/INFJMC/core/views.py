01f1361d0521acc486343e315f15880c9c94cc79
Csarabia2002 <sarabia200212@gmail.com>
2023-04-28 12:04:03 -0400

    App y templates

--- LAB/INFJMC/core/views.py
+++ LAB/INFJMC/core/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home(request):
+    #return HttpResponse("<h1>Inicio</h1>")
+    return render(request,'core/home.html')
+    
+
+def carreras(request):
+    #return HttpResponse("<h1>Carreras</h1>")
+    return render(request,'core/carreras.html')
+
+def docentes(request):
+    #return HttpResponse("<h1>docentes</h1>")
+    return render(request,'core/docentes.html')
